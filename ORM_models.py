bbe54c49c0edc8df2eb739c2e2ec7c515a7e0e9d
Lev <tab.lev.008@gmail.com>
2024-03-28 16:30:45 +0300

    метод добавления рецензий

--- ORM_models.py
+++ ORM_models.py
@@ -43,20 +43,45 @@ def create_user(name, email, password):
     db_sess.commit()
 
 
+def create_review(text, user_id, raiting):
+    review = Review()
+    review.text = text
+    review.user_id = user_id
+    review.raiting = raiting
+    db_sess = create_session()
+    db_sess.add(review)
+    user = db_sess.query(User).filter(User.id == user_id).first()
+    user.reviews_num += 1
+    db_sess.commit()
+
+
 class User(SqlAlchemyBase):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
                            primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String)
     role = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     email = sqlalchemy.Column(sqlalchemy.String,
-                              index=True, unique=False, nullable=True)
+                              index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
     reviews_num = sqlalchemy.Column(sqlalchemy.Integer, default=0)
     reviews_raiting = sqlalchemy.Column(sqlalchemy.Float, default=0)
 
-global_init("data/base.sqlite3")
+class Review(SqlAlchemyBase):
+    __tablename__ = 'reviews'
 
-create_user('Bob', 'Poland_guy', 'Polska')
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    text = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    user_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                sqlalchemy.ForeignKey("users.id"))
+    raiting = sqlalchemy.Column(sqlalchemy.Float)
+    user = orm.relationship('User')
+
+global_init("data/base.sqlite3")
+create_user('Bob', 'bimba', '123')
+create_user('Bob', 'bimba2', '123')
+create_review('nice', 1, 0.2)
+create_review('nice', 2, 0.7)
